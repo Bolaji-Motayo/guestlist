f1d23c1bec45d0b0b4202f9d38ba680f3f1bf08e
Bolaji Motayo <105310813+Bolaji-Motayo@users.noreply.github.com>
2022-05-26 20:04:03 +0100

    Create guest.js

---
+++ guest.js
@@ -0,0 +1,20 @@
+guestName = prompt("What is your name?:")
+
+var guestList = ["Angela", "Ben", "Simon", "Peter", "Joy", "Brian"];
+alert(guestList.includes("guestName"))
+
+
+
+var guestListB = ["Angela", "Ben", "Simon", "Luke", "Joy", "Brian"];
+guestListB.includes("Bay")
+alert (guestList[3]);
+
+var guestName = prompt("What is your name?:")
+
+var guestList = ["Angela", "Ben", "Simon", "Peter", "Joy", "Brian"];
+
+if (guestList.includes("guestName")) {
+    alert ("You are welcome!")
+} else {
+    alert ("Sorry, maybe next time!")
+}
